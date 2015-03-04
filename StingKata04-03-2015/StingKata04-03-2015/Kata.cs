cc900420a9f6b6b64e61cb6aed78549c6fd2ffd5
unknown <Mahesh@Mahesh.chilli.local>
2015-03-04 14:40:45 +0200

    FirstCompletedStrinKata

---
+++ StingKata04-03-2015/StingKata04-03-2015/Kata.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StingKata04_03_2015
+{
+    public class NegativesNotAllowed : Exception
+    {
+       public List<int> negativenumbers { get; set; } 
+    }
+    public partial class Kata
+    {
+        public int Add(string input) {
+            if(input=="")
+                return 0;
+
+
+            if (StartsWith(input))
+            {
+                input = StringSanitizer(input);
+            }
+
+            var s = SplitStrings(input);
+
+            var convertednumbers = Convertednumbers(s);
+            var getnegativenumbers = Getnegativenumbers(convertednumbers);
+            if (Any(getnegativenumbers))
+            {
+                ThrowNegativesNotAllowedException(getnegativenumbers);
+            }
+
+            return SumIntegers(convertednumbers);
+
+
+        }
+
+        private static void ThrowNegativesNotAllowedException(List<int> getnegativenumbers) {
+            var ex = new NegativesNotAllowed();
+            ex.negativenumbers = getnegativenumbers;
+            throw ex;
+        }
+
+        private static List<int> Getnegativenumbers(List<int> convertednumbers) {
+            List<int> getnegativenumbers = convertednumbers.Where(x => x < 0).ToList();
+            return getnegativenumbers;
+        }
+
+        private static bool Any(List<int> getnegativenumbers) {
+            return getnegativenumbers.Any();
+        }
+
+        private static string StringSanitizer(string input) {
+
+            var splitnewline = input.Split('\n');
+            input = splitnewline[1];
+
+            var withoustslash = splitnewline[0].Substring(2);
+            var replacebrackets = withoustslash.Replace("[", ",").Replace("]", ",");
+            var delimieter = replacebrackets.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
+
+
+            foreach (var del in delimieter)
+            {
+
+                input = input.Replace(del, ",");
+            }
+            
+
+
+            return input;
+        }
+
+        private static bool StartsWith(string input) {
+            return input.StartsWith("//");
+        }
+
+        private static int SumIntegers(List<int> convertednumbers) {
+            return convertednumbers.Where(x=>x<=1000).ToList().Sum();
+        }
+
+        private static List<int> Convertednumbers(string[] s) {
+            List<int> convertednumbers = s.ToList().Select(x => Int32.Parse(x)).ToList();
+            return convertednumbers;
+        }
+
+        private static string[] SplitStrings(string input) {
+            char[] c = new[] {',','\n'};
+            string[] s = input.Split(c);
+            return s;
+        }
+    }
+}
---
+++ StingKata04-03-2015/StingKata04-03-2015/StringKata.Tests.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StingKata04_03_2015
+{
+   [TestFixture]
+   public class StringKata
+    {
+       [Test]
+       public void Add_GivenEmptyString_ShouldReturn0() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(0,result);
+       }
+       [Test]
+       public void Add_GivenNumber_ShouldReturnIntegerNumber() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("1");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(1,result);
+       }
+       [Test]
+       public void Add_GivenNumberPartA_ShouldReturnIntegerNumber() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("2");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(2,result);
+       }
+       [Test]
+       public void Add_GivenTwoNumbersSeperatedByComma_ShouldReturnSum() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("1,2");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(3,result);
+       }
+       [Test]
+       public void Add_GivenTwoNumbersSeperatedByCommaPartA_ShouldReturnSum() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("11,22");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(33,result);
+       }
+       [Test]
+       public void Add_GivenTwoNumbersSeperatedByNewline_ShouldReturnSum() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("1,2\n3");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(6,result);
+       }
+       [Test]
+       public void Add_GivenNumbersGreaterThan1000_ShouldNotIncludeNumbers() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("1,2,1002");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(3,result);
+       }
+       [Test]
+       public void Add_GivenNumbersGreaterThan1000PartA_ShouldNotIncludeNumbers() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("1,2,1000");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(1003,result);
+       }
+       [Test]
+       public void Add_GivenTwoNumbersSeperatedByCustomeDelimiter_ShouldReturnSum() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("//;\n1;2;3");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(6,result);
+       }
+       [Test]
+       public void Add_GivenTwoNumbersSeperatedByCustomeDelimiterWithVaryingLength_ShouldReturnSum() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("//[***]\n1***2***3");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(6,result);
+       }
+       [Test]
+       public void Add_GivenTwoNumbersSeperatedByVaryingCustomeDelimiterWithVaryingLength_ShouldReturnSum() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+           //---------------Assert Precondition----------------
+           var result = kata.Add("//[*][%]\n1*2%3");
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+           Assert.AreEqual(6,result);
+       }
+       [Test]
+       public void Add_GivenNegativeNumber_ShouldThrowException() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+         List<int> expected =new List<int>() {-1};
+           ;
+           //---------------Assert Precondition----------------
+          
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+            var exception= Assert.Throws<NegativesNotAllowed>(()=>kata.Add("-1"));
+           CollectionAssert.AreEqual(expected,exception.negativenumbers);
+
+       }
+       [Test]
+       public void Add_GivenNegativeNumberList_ShouldThrowException() {
+           //---------------Set up test pack-------------------
+           
+           Kata kata=new Kata();
+         List<int> expected =new List<int>() {-1,-2,-4};
+           ;
+           //---------------Assert Precondition----------------
+          
+           //---------------Execute Test ----------------------
+
+           //---------------Test Result -----------------------
+            var exception= Assert.Throws<NegativesNotAllowed>(()=>kata.Add("-1,2,-2,-4"));
+           CollectionAssert.AreEqual(expected,exception.negativenumbers);
+
+       }
+
+
+
+
+
+
+    }
+}
