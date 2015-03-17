601d471d6f5a321b3b44d7ce2cc3b867400c28ce
unknown <Mahesh@Mahesh.chilli.local>
2015-03-17 15:15:15 +0200

    Latest Katarai Added

---
+++ StringCalculator-2015_03_17_14_42_31/PlayerSolution/StringCalculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Katarai.StringCalculator.Interfaces;
+using NUnit.Framework;
+
+namespace PlayerStringKata
+{
+    public class StringCalculator : IStringCalculator
+    {
+        public int Add(string input) {
+            if (input == "")
+                return 0;
+
+            if (input.StartsWith("//"))
+            {
+                input = StringSanitizer(input);
+            }
+            var s = SplitStrings(input);
+            
+            var convertednumbers = Convertednumbers(s);
+            
+            ThrowNegativesException(convertednumbers);
+
+            return Sum(convertednumbers);
+        }
+
+        private static void ThrowNegativesException(List<int> convertednumbers) {
+            List<int> negativenumbers = convertednumbers.Where(x => x < 0).ToList();
+            if (negativenumbers.Any())
+            {
+                throw new NegativesNotAllowedException(negativenumbers.ToArray());
+            }
+        }
+
+        private static string StringSanitizer(string input) {
+            var splitnewline = Splitnewline(input);
+
+            input = splitnewline[1];
+
+            var removeslashes = Removeslashes(splitnewline);
+
+            var removebrackets = removeslashes.Replace('[', ',').Replace(']', ',');
+
+            var customdelimiter = removebrackets.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < customdelimiter.Length; i++)
+            {
+
+                input = Input(input,customdelimiter[i]);
+
+            }
+
+            return input;
+        }
+
+        private static string Input(string input, string customdelimiter) {
+            input = input.Replace(customdelimiter, ",");
+            return input;
+        }
+
+        private static string Removeslashes(string[] splitnewline) {
+            var removeslashes = splitnewline[0].Substring(2);
+            return removeslashes;
+        }
+
+        private static string[] Splitnewline(string input) {
+            var splitnewline = input.Split('\n');
+            return splitnewline;
+        }
+
+        private static int Sum(List<int> convertednumbers) {
+            return convertednumbers.Where(x=>x<=1000).Sum();
+        }
+
+        private static List<int> Convertednumbers(string[] s) {
+            List<int> convertednumbers = s.Select(x => Int32.Parse(x)).ToList();
+            return convertednumbers;
+        }
+
+        private static string[] SplitStrings(string input) {
+            string[] s = input.Split(',','\n');
+            return s;
+        }
+    }
+}
---
+++ StringCalculator-2015_03_17_14_42_31/PlayerSolution/TestStringCalculator.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using Engine;
+using Katarai.StringCalculator.Interfaces;
+using NUnit.Framework;
+
+namespace PlayerStringKata
+{
+    public class TestStringCalculator : ITestPack<IStringCalculator>
+    {
+        public Func<IStringCalculator> CreateSUT { get; set; }
+
+		[SetUp]
+        public void SetupTest()
+        {
+            CreateSUT = () => new StringCalculator();
+        }
+		
+		private IStringCalculator CreateCalculator()
+		{
+			return CreateSUT();
+		}
+		
+        /// <summary>
+        /// This is a sample test that shows how the StringCalculator 
+        /// should be created in future tests. 
+        /// </summary>
+        [Test]
+        public void Constructor()
+        {
+            //---------------Arrange-------------------
+            
+            //---------------Act----------------------
+            var calculator = CreateCalculator();
+            //---------------Assert-----------------------
+            Assert.IsNotNull(calculator);
+        }
+
+        [Test]
+        public void Add_GivenEmptyString_ShouldReturn0() {
+            //---------------Set up test pack-------------------
+            var input = "";
+            var expected = 0;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenStringNumber_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "1";
+            var expected = 1;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenStringNumber2_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "2";
+            var expected = 2;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenStringNumbersSeperatedByComma_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "1,2";
+            var expected = 3;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenDoubleDigitsStringNumbersSeperatedByComma_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "11,22";
+            var expected = 33;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenThreeNumbersSeperatedByComma_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "1,2,3";
+            var expected = 6;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenThreeNumberswithNumberGreaterThan1000_ShouldNotReturnSumWithNumberGreaterThan1000() {
+            //---------------Set up test pack-------------------
+            var input = "1,2,1001";
+            var expected = 3;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenThreeNumberswithNumbeEqualThan1000_ShouldNotReturnSumWithNumberEqual1000() {
+            //---------------Set up test pack-------------------
+            var input = "1,2,1000";
+            var expected = 1003;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenThreeNumbersSeperatedByNewLine_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "1,2\n3";
+            var expected = 6;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenThreeNumbersSeperatedByCustomDelimiter_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "//;\n1;2;3";
+            var expected = 6;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenThreeNumbersSeperatedByCustomDelimiterofVaryingLength_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "//[***]\n1***2***3";
+            var expected = 6;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenThreeNumbersSeperatedByCustomDelimiterofVaryingLengthandType_ShouldReturnIntegerValue() {
+            //---------------Set up test pack-------------------
+            var input = "//[*][%]\n1*2%3";
+            var expected = 6;
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = sut.Add(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+        [Test]
+        public void Add_GivenNegativeNumber_ShouldThrowNegativesNotAllowedException() {
+            //---------------Set up test pack-------------------
+            var input = "-1";
+           
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            //---------------Test Result -----------------------
+            Assert.Throws<NegativesNotAllowedException>(()=>sut.Add(input));
+        }
+        [Test]
+        public void Add_GivenNegativeNumberList_ShouldThrowNegativesNotAllowedException() {
+            //---------------Set up test pack-------------------
+            var input = "-1";
+           List<int> expected=new List<int>(){-1};
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            //---------------Test Result -----------------------
+           var exception= Assert.Throws<NegativesNotAllowedException>(()=>sut.Add(input));
+            CollectionAssert.AreEqual(expected,exception.NegativeNumbers);
+        }
+        [Test]
+        public void Add_GivenNegativeNumberList_ShouldThrowNegativesMotAllowedException() {
+            //---------------Set up test pack-------------------
+            var input = "-1,-2";
+           List<int> expected=new List<int>(){-1,-2};
+            var sut = CreateCalculator();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            //---------------Test Result -----------------------
+           var exception= Assert.Throws<NegativesNotAllowedException>(()=>sut.Add(input));
+            CollectionAssert.AreEqual(expected,exception.NegativeNumbers);
+        }
+
+    }
+}
