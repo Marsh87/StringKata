56b788f2ca57c00d7d989614b685c11d3e5f9987
unknown <Mahesh@Mahesh.chilli.local>
2015-03-19 11:04:11 +0200

    Latest katarai Added

---
+++ StringCalculator-2015_03_19_10_14_05/PlayerSolution/StringCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Katarai.StringCalculator.Interfaces;
+using NUnit.Framework;
+
+namespace PlayerStringKata
+{
+    public class StringCalculator : IStringCalculator
+    {
+        public int Add(string input) {
+            if(input=="")
+                return 0;
+
+            if (StartsWith(input))
+            {
+                input = StringSanitizer(input);
+            }
+
+            var s = SplitStrings(input);
+
+            var convertedNumbers = ConvertedNumbers(s);
+    
+            NegativesNotAllowedException(convertedNumbers);
+
+
+            return SumIntegers(convertedNumbers);
+        }
+
+        private static void NegativesNotAllowedException(List<int> convertedNumbers) {
+            List<int> negativenumbers = convertedNumbers.Where(x => x < 0).ToList();
+            if (negativenumbers.Any())
+            {
+                throw new NegativesNotAllowedException(negativenumbers.ToArray());
+            }
+        }
+
+        private static bool StartsWith(string input) {
+            return input.StartsWith("//");
+        }
+
+        private static string StringSanitizer(string input) {
+            var splitnewline = input.Split('\n');
+            input = splitnewline[1];
+
+            var removeslashes = splitnewline[0].Substring(2);
+
+            var removebrackets = removeslashes.Replace('[', ',').Replace(']', ',');
+
+            var customdelimiter = removebrackets.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i <customdelimiter.Length; i++)
+            {
+                input = input.Replace(customdelimiter[i], ",");    
+            }
+            
+
+
+            return input;
+        }
+
+        private static int SumIntegers(List<int> convertedNumbers) {
+            return convertedNumbers.Where(x=>x<=1000).Sum();
+        }
+
+        private static List<int> ConvertedNumbers(string[] s) {
+            List<int> convertedNumbers = s.Select(x => Int32.Parse(x)).ToList();
+            return convertedNumbers;
+        }
+
+        private static string[] SplitStrings(string input) {
+            string[] s = input.Split(',', '\n');
+            return s;
+        }
+    }
+}
